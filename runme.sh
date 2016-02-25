94160fc79b519e97ce03e3648f6653a0a58b30c7
Jordi Prats <jprats@systemadmin.es>
2016-02-25 13:27:25 +0100

    afegida opció de IP

--- runme.sh
+++ runme.sh
@@ -2,6 +2,11 @@
 
 exec > /tmp/last.puppet.run.log 2>&1
 
+if [ ! -z "${EYP_PUPPET_HOST_IP}" ];
+then
+  echo "${EYP_PUPPET_HOST_IP} ${EYP_PUPPET_HOST}" >> /etc/hosts
+fi
+
 mkdir -p ${EYP_IDDIR}
 echo ${EYP_ENV} > ${EYP_IDDIR}/${EYP_IDFILE}
 
