eb82b34712366e61c1020cabfbc22b9b11096dd1
Jordi Prats <jprats@systemadmin.es>
2016-02-29 18:38:51 +0100

    afegits canvis per netejar el diff

--- runme.sh
+++ runme.sh
@@ -11,3 +11,7 @@ mkdir -p ${EYP_IDDIR}
 echo ${EYP_ENV} > ${EYP_IDDIR}/${EYP_IDFILE}
 
 puppet agent --server=${EYP_PUPPET_HOST} --waitforcert=30 --no-daemonize --verbose --onetime --pluginsync --masterport=${EYP_PUPPET_PORT}
+
+yum clean all
+
+rm -fr /var/lib/puppet /var/log
