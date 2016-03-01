ec420d78f331b8e39b8a43ab6036e53c8711a50d
Jordi Prats <jprats@systemadmin.es>
2016-03-01 12:44:33 +0100

    afegit debug

--- runme.sh
+++ runme.sh
@@ -12,8 +12,13 @@ fi
 mkdir -p ${EYP_IDDIR}
 echo ${EYP_ENV} > ${EYP_IDDIR}/${EYP_IDFILE}
 
+echo "=== /etc/hosts =="
+cat /etc/hosts
+
+echo "=== facts =="
 facter -p
 
+echo "=== puppet agent =="
 puppet agent --server=${EYP_PUPPET_HOST} --waitforcert=30 --no-daemonize --verbose --onetime --pluginsync --masterport=${EYP_PUPPET_PORT}
 
 yum clean all
