899407ffd3a39e31aebb6b90a689c497d211a5a2
Jordi Prats <jprats@systemadmin.es>
2016-03-01 13:15:02 +0100

    afegit facts post run

--- runme.sh
+++ runme.sh
@@ -21,6 +21,9 @@ facter -p
 echo "=== puppet agent =="
 puppet agent --server=${EYP_PUPPET_HOST} --waitforcert=30 --no-daemonize --verbose --onetime --pluginsync --masterport=${EYP_PUPPET_PORT}
 
+echo "=== facts =="
+facter -p
+
 yum clean all
 
 rm -fr /var/lib/puppet
