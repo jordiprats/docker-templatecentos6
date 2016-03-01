d7dc53fe6844f71ed76997413a272ba3f88dbd44
Jordi Prats <jprats@systemadmin.es>
2016-03-01 12:41:52 +0100

    afegit facter

--- runme.sh
+++ runme.sh
@@ -12,6 +12,8 @@ fi
 mkdir -p ${EYP_IDDIR}
 echo ${EYP_ENV} > ${EYP_IDDIR}/${EYP_IDFILE}
 
+facter -p
+
 puppet agent --server=${EYP_PUPPET_HOST} --waitforcert=30 --no-daemonize --verbose --onetime --pluginsync --masterport=${EYP_PUPPET_PORT}
 
 yum clean all
